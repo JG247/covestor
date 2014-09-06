7613f910b43a35a67d9cea7c2c3704b498d4f5f3
James <james@jjgportfolio.com>
2014-09-06 09:08:27 +0100

    Added grunt, karma and jasmine

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+/**
+ * Created by jgpro_000 on 06/09/14.
+ */
+module.exports = function(grunt) {
+    // Do grunt-related things in here
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.initConfig({
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                background: true
+            }
+        },
+        watch: {
+            //run unit tests with karma (server needs to be already running)
+            karma: {
+                files: ['app/js/**/*.js', 'js/tests/*spec.js'],
+                tasks: ['karma:unit:run'] //NOTE the :run flag
+            }
+        }
+    });
+};
