33a239e9596d947f81dd6e4fb93069cb18a32c32
James <james@jjgportfolio.com>
2014-09-08 22:22:37 +0100

    Return new random panel data

--- js/models/DataModel.js
+++ js/models/DataModel.js
@@ -21,6 +21,10 @@ define([
             quotes : "panel quote QUOTES",
             multiManager: "panel mmp MMPPANELS",
             manager: "panel moneyManager MANAGERPANEL"
+        },
+        delay : {
+            min : 1000,
+            max : 2000
         }
     };
     function DataModel () {}
@@ -45,53 +49,71 @@ define([
         var getJsonConstructor = new DataModel();
         getJsonConstructor.getJsonConstructor('js/data/data.json',
             function(data) {
-                var saveDataToLocalStorage = new DataModel();
-                saveDataToLocalStorage.saveDataToLocalStorage(data);
+                var dataModel = new DataModel();
+                dataModel.selectNewPanelData(data);
             },
             function(xhr) {
                 console.error(xhr);
             });
     };
-    DataModel.prototype.checkLocalStorage = function () {
-        if (localStorage != "undefined"){
-            return true;
-        } else {
-            return false;
-        }
-    };
-    DataModel.prototype.saveDataToLocalStorage = function (data) {
-        this.data = data;
-        var checkLocalStorage = new DataModel();
-        if (checkLocalStorage.checkLocalStorage() == true) {
-            window.localStorage.setItem("Data", JSON.stringify(data));
-        } else {
-            // browser not supported
-        }
-    };
-    DataModel.prototype.data = window.localStorage.getItem("Data");
     DataModel.prototype.selectRandomPanel = function () {
         var panels = document.getElementsByClassName('panel'),
         randomPanelPosition = Math.floor((Math.random() * panels.length) + 0),
         randomPanel = panels[randomPanelPosition],
         randomPanelType = randomPanel.attributes.class.value,
-        randomPanelData = [{"randomPanel" : randomPanel}, {"randomPanelPosition": randomPanelPosition}, {"randomPanelType" : randomPanelType}],
+        randomPanelId = randomPanel.attributes[1].value,
+        randomPanelData = [{"randomPanel" : randomPanel}, {"randomPanelPosition": randomPanelPosition}, {"randomPanelType" : randomPanelType},{"randomPanelId" : randomPanelId}],
         o = options;
         if ( randomPanelType == o.panelTypes.quotes ) {
-            var id = randomPanel.children[0].children[0].textContent;
-            randomPanelData.push({"id": id});
             return randomPanelData;
         } else if ( randomPanelType == o.panelTypes.manager ) {
-            var id = randomPanel.children[1].children[0].textContent;
-            randomPanelData.push({"id": id});
             return randomPanelData;
         } else if ( randomPanelType == o.panelTypes.multiManager ) {
-            var id = randomPanel.children[1].textContent;
-            randomPanelData.push({"id": id});
             return randomPanelData;
         }
     };
+    DataModel.prototype.triggerNewPanelData = function () {
+        window.setInterval(function () { dataModel.getJson()}, 2000);
+    };
+    DataModel.prototype.selectNewPanelData = function (data) {
+        var dataModel = new DataModel();
+        var selectedRandomPanelData = dataModel.selectRandomPanel();
+        if ( selectedRandomPanelData[2].randomPanelType ==  "panel quote QUOTES") {
+            var quotes = data.QUOTES,
+            id = selectedRandomPanelData[3].randomPanelId;
+            for (var i = 0; i < quotes.length; i++) {
+                if ( quotes[i].id == id) {
+                    quotes.splice(i,1);
+                    var newSelectedQuote = Math.floor((Math.random() * quotes.length  ) + 0),
+                    newQuote = quotes[newSelectedQuote];
+                    return newQuote;
+                }
+            }
+        } else if ( selectedRandomPanelData[2].randomPanelType == "panel moneyManager MANAGERPANEL") {
+            var managers = data.MANAGERPANEL,
+            id = selectedRandomPanelData[3].randomPanelId;
+            for (var i = 0; i < managers.length; i++) {
+                if ( managers[i].id == id) {
+                    managers.splice(i,1);
+                    var newSelectedManager = Math.floor((Math.random()* managers.length) + 0 ),
+                    newManager = managers[newSelectedManager];
+                    return newManager;
+                }
+            }
+        } else {
+            var multiManagers = data.MMPPANELS,
+            id = selectedRandomPanelData[3].randomPanelId;
+            for (var i = 0; i < multiManagers.length; i++) {
+                if ( multiManagers[i].id == id) {
+                    multiManagers.splice(i,1);
+                    var newSelectedMultiManager = Math.floor((Math.random()* multiManagers.length) + 0 ),
+                    newMultiManager = multiManagers[newSelectedMultiManager];
+                    return newMultiManager;
+                }
+            }
+        }
+    };
     var dataModel = new DataModel();
-    dataModel.getJson();
-    dataModel.selectRandomPanel();
+    dataModel.triggerNewPanelData();
     return DataModel;
 });
